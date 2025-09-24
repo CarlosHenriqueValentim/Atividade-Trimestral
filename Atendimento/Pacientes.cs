d36371e9e66f5e026199f8b296388f3cf3f96134
Carlos Henrique Valentim Silva <rikcar22@gmail.com>
2025-09-23 23:36:50 -0300

    Update Pacientes.cs

--- Atendimento/Pacientes.cs
+++ Atendimento/Pacientes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Atendimento15Pacientes
 {
-    internal class Pacientes
+    class Pacientes
     {
         public string nome;
         public int numpaciente;
