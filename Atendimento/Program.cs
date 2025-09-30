26fbd7498d5c7f80d9ee4123f5ed0eb654d9a308
Carlos Henrique Valentim Silva <rikcar22@gmail.com>
2025-09-30 17:12:54 -0300

    Update Program.cs

--- Atendimento/Program.cs
+++ Atendimento/Program.cs
@@ -12,14 +12,14 @@ namespace Atendimento15Pacientes
             string escolher;
             int tamanho = 15;
             int auxiliar = 0;
-            Pacientes[] Paciente = new Pacientes[tamanho];
-            while (true) // Loop Infinito sempre do Menu de Opçôes 
-            { 
+            Pacientes[] TamanhodoPaciente = new Pacientes[tamanho];
+            while (true)
+            {
                 Console.Write("Menu Atendimento\n\nCadastrar - 1\nLista de Pacientes - 2 \nAtender - 3\nAlterar Dados - 4\nSair - Q\n\nEscolha: ");
                 escolher = Console.ReadLine();
                 switch (escolher)
                 {
-                    case "1": // Cadastrar Paciente
+                    case "1":
                         if (auxiliar < tamanho)
                         {
                             Pacientes NovoPaciente = new Pacientes();
@@ -29,13 +29,13 @@ namespace Atendimento15Pacientes
                             {
                                 for (int i = auxiliar; i > 0; i--)
                                 {
-                                    Paciente[i] = Paciente[i - 1];
+                                    TamanhodoPaciente[i] = TamanhodoPaciente[i - 1];
                                 }
-                                Paciente[0] = NovoPaciente;
+                                TamanhodoPaciente[0] = NovoPaciente;
                             }
                             else
                             {
-                                Paciente[auxiliar] = NovoPaciente;
+                                TamanhodoPaciente[auxiliar] = NovoPaciente;
                             }
                             auxiliar++;
                             Console.WriteLine("\nPaciente cadastrado com sucesso!\n");
@@ -45,7 +45,7 @@ namespace Atendimento15Pacientes
                             Console.WriteLine("\nFila cheia.\n");
                         }
                         break;
-                    case "2": // Lista de Pacientes
+                    case "2":
                         Console.WriteLine("\nLista de Pacientes");
                         if (auxiliar == 0)
                         {
@@ -56,31 +56,31 @@ namespace Atendimento15Pacientes
                             for (int i = 0; i < auxiliar; i++)
                             {
                                 Console.Write(i + 1 + " - ");
-                                Paciente[i].MostrarDados();
+                                TamanhodoPaciente[i].MostrarDados();
                             }
                         }
                         break;
-                    case "3": // Atender Pacientes
+                    case "3":
                         if (auxiliar == 0)
                         {
                             Console.WriteLine("\nNenhum paciente na fila.\n");
                         }
                         else
                         {
-                            Console.WriteLine("\nAtendendo" + Paciente[0].nome);
+                            Console.WriteLine("\nAtendendo" + TamanhodoPaciente[0].nome);
                             for (int i = 0; i < auxiliar - 1; i++)
                             {
-                                Paciente[i] = Paciente[i + 1];
+                                TamanhodoPaciente[i] = TamanhodoPaciente[i + 1];
                             }
-                            Paciente[auxiliar - 1] = null; auxiliar--;
+                            TamanhodoPaciente[auxiliar - 1] = null; auxiliar--;
                         }
                         break;
-                    case "4": // Alterar Cadastrais do Paciente 
+                    case "4":
                         Console.Write("\nDigite o número do paciente para alterar:\n");
                         int posição = int.Parse(Console.ReadLine()) - 1;
                         if (posição < auxiliar)
                         {
-                            Paciente[posição].CadastrarPaciente();
+                            TamanhodoPaciente[posição].CadastrarPaciente();
                             Console.WriteLine("\nDados alterados com sucesso\n");
                         }
                         else
@@ -88,13 +88,13 @@ namespace Atendimento15Pacientes
                             Console.WriteLine("\nPaciente não encontrado\n");
                         }
                         break;
-                    case "Q": // Saida da Depuração
+                    case "Q":
                         Console.WriteLine("\nDepuração Finalizada\n");
                         return;
-                    case "q": // Saida da Depuração com q minusculo
+                    case "q":
                         Console.WriteLine("\nDepuração Finalizada\n");
                         return;
-                    default: // Qualquer tecla que o usuario digitar
+                    default:
                         Console.WriteLine("\nOpção incorreta, Digite outra Opção\n");
                         break;
                 }
