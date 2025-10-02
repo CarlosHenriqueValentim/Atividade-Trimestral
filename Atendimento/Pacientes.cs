ad7259138a0f8b87e3e798d7eeb25d7d8eb8ff07
Carlos Henrique Valentim Silva <rikcar22@gmail.com>
2025-10-02 11:04:55 -0300

    COMMIT Organizando

--- Atendimento/Pacientes.cs
+++ Atendimento/Pacientes.cs
@@ -10,31 +10,19 @@ namespace Atendimento15Pacientes
     {
         public string nome;
         public int numeropaciente;
-        public int idade;
         public int preferencial;
 
         public void CadastrarPaciente()
-        {
-            Console.Write("\nDigite o Nome do Paciente:");
-            nome = Console.ReadLine();
+        {Console.Write("\nDigite o Nome do Paciente:");
+         nome = Console.ReadLine();
 
-            Console.Write("\nDigite a Idade do Paciente:");
-            idade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("\nDigite o nivel Preferencial do Paciente\n1 - Baixo\n2- Médio\n3 - Alto");
-            preferencial = int.Parse(Console.ReadLine());
-        }
+         Console.Write("\nDigite o nivel Preferencial do Paciente\n\n1 - Baixo\n2- Médio\n3 - Alto\n\nEscolha:");
+         preferencial = int.Parse(Console.ReadLine());}
 
         public void MostrarDados()
-        {
-            Console.WriteLine("{0}, {1} anos, Nivel Preferêncial: {2}", nome, idade, preferencial);
-        }
+        {Console.WriteLine("{0} - Nivel Preferêncial: {1}", nome, preferencial);}
 
         public void Atendimento()
-
-        {
-            Console.WriteLine("Atendendo Paciente {0}", nome);
-        }
-
+        {Console.WriteLine("Atendendo Paciente {0}", nome);}
     }
 }
